cebcfa8460b02c3c9582397b46c820f97f57bd8b
sizhe233 <117633756+sizhe233@users.noreply.github.com>
2024-08-23 01:34:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.sfol.top": dockerHub,
+  "quay.sfol.top": "https://quay.io",
+  "gcr.sfol.top": "https://gcr.io",
+  "k8s-gcr.sfol.top": "https://k8s.gcr.io",
+  "k8s.sfol.top": "https://registry.k8s.io",
+  "ghcr.sfol.top": "https://ghcr.io",
+  "cloudsmith.sfol.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.sfol.top": dockerHub,
 };
 
 function routeByHosts(host) {
